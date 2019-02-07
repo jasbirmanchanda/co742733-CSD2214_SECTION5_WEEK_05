979d2ab85399981c06f4cb2175cf39b7c760d267
visualstudio <visualstudio@230-PC25>
2019-02-07 11:55:31 -0500

    final change

--- co742733/js/main.js
+++ co742733/js/main.js
@@ -5,6 +5,12 @@
 
 function arrays() {
     var pens;
-    pens = ["red", "orange", "purple", "green", "merjanta"];
+    pens = new Array("red", "orange", "purple", "green", "merjanta");
     console.log(pens);
+    console.log(pens[0]);
+
+    for (i = 0; i < pens.length; i++) {
+        alert(pens[i]);
+    }
+    alert("Length od array is:"+ pens.length);
 }
