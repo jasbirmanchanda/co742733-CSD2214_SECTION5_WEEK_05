ad8d460b193c18e189dfa00b3aab6c670b36953d
visualstudio <visualstudio@230-PC25>
2019-02-07 11:45:11 -0500

    changes

--- co742733/js/main.js
+++ co742733/js/main.js
@@ -1,1 +1,10 @@
-﻿// Your code here!
+﻿function f1(a, b) {
+   
+    return (a + b);  // The  function returns the product of p1 and p2
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "purple", "green", "merjanta"];
+    console.log(pens);
+}
